1f91078b0fe0437c1e204744278cbf60b26c5a23
Corbin Goetsch <goetschc7886@my.uwstout.edu>
2023-04-27 09:52:18 -0500

    player class

--- src/application/Player.java
+++ src/application/Player.java
@@ -2,4 +2,35 @@ package application;
 
 public class Player {
 
+	private int xpos;
+	private int ypos;
+	
+	public Player() {
+		xpos=0;
+		ypos=0;
+	}
+	
+	public void moveUp() {
+		ypos--;
+	}
+
+	public void moveDown() {
+		ypos++;
+	}
+
+	public void moveLeft() {
+		xpos--;
+	}
+
+	public void moveRight() {
+		xpos++;
+	}
+	
+	public int getY() {
+		return ypos;
+	}
+	
+	public int getX() {
+		return xpos;
+	}
 }
