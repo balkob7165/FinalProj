014ad88e43f05f8735d796b2535c7bbbf2a4778a
Corbin Goetsch <goetschc7886@my.uwstout.edu>
2023-04-27 11:16:09 -0500

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application {
 			for(int i = 0;i<10;i++) {
 				for(int j = 0;j<10;j++) {
 					Map mapdata = new Map();
-					mapdata.gernerate();
+					mapdata.generate();
 					map[i][j]= mapdata;
 				}
 			}
