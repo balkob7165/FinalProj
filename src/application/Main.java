87bc7c02e229445d92c9c77ba62b5a8489510c3c
Corbin Goetsch <goetschc7886@my.uwstout.edu>
2023-04-30 16:53:17 -0500

    Image call

--- src/application/Main.java
+++ src/application/Main.java
@@ -14,13 +14,22 @@ public class Main extends Application {
 		try {
 			BorderPane root = new BorderPane();
 			
-			//adds background image
-			//Image bg = new Image("Assets/Grassfield.png");
+			//adds images
 			Image bg = new Image(getClass().getResource("/Assets/Grassfield.png").toString(), true);
-			ImageView img = new ImageView(bg);
-			root.getChildren().add(img); 
+			ImageView bg1 = new ImageView(bg);
+			Image coin = new Image(getClass().getResource("/Assets/Coin.png").toString(), true);
+			ImageView coin1 = new ImageView(coin);
+			Image goblin = new Image(getClass().getResource("/Assets/Goblin.png").toString(), true);
+			ImageView goblin1 = new ImageView(goblin);
+			Image player = new Image(getClass().getResource("/Assets/Player.png").toString(), true);
+			ImageView player1 = new ImageView(player);
+			Image tree = new Image(getClass().getResource("/Assets/TreeSprite.png").toString(), true);
+			ImageView tree1 = new ImageView(tree);
 			
-			Scene scene = new Scene(root,600,400);
+			
+			root.getChildren().add(bg1); 
+			
+			Scene scene = new Scene(root,480,320);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
